4e6db18fd9df00bbd5dfbca87837d55f578199ff
SamiTheDev9 <samithedev9@gmail.com>
2023-01-26 22:51:15 +0500

    Information Security Assignment Teacher sir Asif Khan

---
+++ app/src/main/java/com/mst/vcs_and_github_assi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mst.vcs_and_github_assi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
